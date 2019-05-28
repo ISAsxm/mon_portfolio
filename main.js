82ceb0e85efb416d865eb79c2128d71823fe1d3c
ISAsxm <huguesisabelle82@gmail.com>
2019-05-28 19:36:42 +0000

    test slider

--- main.js
+++ main.js
@@ -152,6 +152,41 @@ $('#toTopBtn').on('click', function(){
 });
 
 
+//test pour slide
+
+var slideIndex = [1,1];
+var slideId = ["mySlides1", "mySlides2"]
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+$('.prev').on('click', function(){
+      plusSlides(-1, 0);
+});
+$('.next').on('click', function(){
+      plusSlides(1, 0);
+});
+$('.prev2').on('click', function(){
+      plusSlides(-1, 1);
+});
+$('.next2').on('click', function(){
+      plusSlides(1, 1);
+});
+
+function showSlides(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
+    
+//fin du test pour slide
 
 // fin du document ready function
 });
